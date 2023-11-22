8f9a02e227406d691b69f20011b1b796fd9fd47e
Daniel Teixeira <daniellhrt@hotmail.com>
2023-11-21 21:28:51 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoAPI = new ConsumoAPI();
+        var json = consumoAPI.obterDados("");
+
+        System.out.println(json);
+
+        ConverteDados converteDados = new ConverteDados();
+        DadosSerie informacoes = converteDados.obterDados(json, DadosSerie.class);
+        System.out.println(informacoes);
+
+    }
+}
